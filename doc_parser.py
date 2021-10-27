d8937b0036065732cecb74d9d552f4d56e634718
Margarida <margarida.martins@ua.pt>
2021-10-27 12:23:50 +0100

    initial parser

---
+++ doc_parser.py
@@ -0,0 +1,10 @@
+class DocParser:
+
+    def __init__(self, filename):
+        self.file=open(filename, "r")
+    
+    def read_file(self):
+        return self.file.read()
+    
+    def close_file():
+        self.file.close()
