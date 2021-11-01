8e5e0583bd4ed36dfba0605684bdd59a78c309e0
Margarida <margarida.martins@ua.pt>
2021-11-01 10:54:03 +0000

    parser init

--- doc_parser.py
+++ doc_parser.py
@@ -1,10 +1,12 @@
+import csv  
+
 class DocParser:
 
     def __init__(self, filename):
         self.file=open(filename, "r")
     
-    def read_file(self):
-        return self.file.read()
+    def read_file_csv(self):
+        return csv.DictReader(self.file,  dialect='unix')
     
     def close_file():
         self.file.close()
