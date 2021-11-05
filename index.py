738f2ffad1ab6f2c0093b1dceef181c2aad73c74
Margarida <margarida.martins@ua.pt>
2021-11-05 17:16:25 +0000

    trying merge files function

--- index.py
+++ index.py
@@ -10,20 +10,33 @@ import os
 
 class Index:
 
-    def write_to_disk(self, out_file, i):
-        with open(out_file, "r+") as f:
+    def merge_files(self, out_file, i):
+        with open(out_file, "w+") as f:
 
             #going through the temp files
-            for n in range(i):
-                sep = str(i) + "."
+            for n in range(i+1):
+                sep = str(n) + "."
                 filename = sep.join(out_file.split('.'))
-
+                
                 #do stuff here
                 with open(filename, "r") as fi:
-                    for line in f:
-                        element = line.split()[0]
-                        while element > fi.split()[0]:  
-                            pass
+                    line_f=f.readline()
+                    line_fi=fi.readline()
+                    while line_fi:
+                        print("Linha ficheiro out:",line_f)
+                        print("Linha ficheiro temp:",line_fi)
+                        print(line_fi.split()[0])
+                        while line_f and line_f.split()[0] <  line_fi.split()[0]:
+                            line_f=f.readline()
+                        
+                        if line_f and line_f.split()[0]== line_fi.split()[0]:
+                            pass 
+                        elif not line_f or  line_f.split()[0]>line_fi.split()[0]:
+                            f.write(line_fi)
+                        
+                        line_fi= fi.readline()
+                f.seek(0)
+
                 
                 #when we're done, removing it
                 os.remove(filename)
@@ -57,6 +70,15 @@ class Index:
                 npostings=0
                 i+=1
 
+        sep = str(i) + "."
+        output_file=open(sep.join(out_file.split('.')), "w")
+        
+        #writing the ordered dict in the file
+        for key in sorted(dictionary.keys()):
+            output_file.write(key + " " + str(dictionary[key]) + "\n")
+        
+        output_file.close()
+
         #merge files
-        self.write_to_disk()
+        self.merge_files(out_file, i)
         return dictionary
